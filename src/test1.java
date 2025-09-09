0d3b7095505b84bf1124f36b4512db9e8bc9dac0
CHYHEE <wnn2011@naver.com>
2025-09-10 00:49:49 +0900

    develop2 수정

--- src/test1.java
+++ src/test1.java
@@ -1,6 +1,6 @@
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("test1");
+        System.out.println("develop2 브랜치에서 수정");
         System.out.println("git stash일 경우 test");
     }
 }
