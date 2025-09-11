1314c70c217e7f9c63edba88e00bd0d6cb350e5c
CHYHEE <wnn2011@naver.com>
2025-09-11 16:13:25 +0900

    merge1

--- src/test2.java
+++ src/test2.java
@@ -1,8 +1,4 @@
+package PACKAGE_NAME;
+
 public class test2 {
-    public static void main(String[] args) {
-        System.out.println("test2");
-        System.out.println("test3");
-        System.out.println("test4");
-        System.out.println("test5");
-    }
 }
